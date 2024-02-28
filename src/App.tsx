ff5dbd47cf840ca37bd89e973fa89a6ce412607f
Nuraiym Nurbulatova <140681537+nooraiym@users.noreply.github.com>
2024-02-28 21:30:00 +0600

    first lesson - 1 sprint

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { TaskType, Todolist } from './Todolist';
+
+
 
 function App() {
+
+let tasks1: Array<TaskType> = [
+  {id: 1, title: "HTML & CSS", isDone: true},
+  {id: 2, title: "JS", isDone: true},
+  {id: 3, title: "React", isDone: false},
+]
+
+let tasks2 = [
+  {id: 1, title: "Bananas", isDone: true},
+  {id: 2, title: "Tomatoes", isDone: true},
+  {id: 3, title: "Cheetos", isDone: false},
+]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist title="What to learn" tasks={tasks1} />
+      <Todolist title="What to buy" tasks={tasks2} />
     </div>
   );
 }
