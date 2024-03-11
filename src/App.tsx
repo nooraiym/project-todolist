212631e32ee0f86b2c9cb7ec25de0afe3a7ca9b1
nooraiym <140681537+nooraiym@users.noreply.github.com>
2024-03-11 19:30:18 +0500

    second lesson - 1 sprint

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { TaskType, Todolist } from './Todolist';
 
 
+export type FilterValuesType = "all" | "active" | "completed"
+
 
 function App() {
 
-let tasks1: Array<TaskType> = [
+let [tasks, setTasks] = useState<Array<TaskType>>([
   {id: 1, title: "HTML & CSS", isDone: true},
   {id: 2, title: "JS", isDone: true},
   {id: 3, title: "React", isDone: false},
-]
+]);
 
-let tasks2 = [
-  {id: 1, title: "Bananas", isDone: true},
-  {id: 2, title: "Tomatoes", isDone: true},
-  {id: 3, title: "Cheetos", isDone: false},
-]
+let [filter, setFilter] = useState<FilterValuesType>("all");
+
+function changeFilter(value: FilterValuesType ) {
+  setFilter(value);
+}
+
+let filteredTasks = tasks;
+if ( filter === "completed") {
+  filteredTasks = tasks.filter( t => t.isDone === true)
+}
+if ( filter === "active") {
+  filteredTasks = tasks.filter( t => t.isDone === false)
+}
+
+function removeTask(id: number) {
+  let resultTasks = tasks.filter( t => t.id !== id);
+  setTasks(resultTasks);
+}
+
+
 
   return (
     <div className="App">
-      <Todolist title="What to learn" tasks={tasks1} />
-      <Todolist title="What to buy" tasks={tasks2} />
+      <Todolist title="What to learn" tasks={filteredTasks} callBack={removeTask} functionForButton={changeFilter} />
     </div>
   );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FilterValuesType } from './App';
 
 export type TaskType = {
   id: number
@@ -9,6 +10,8 @@ export type TaskType = {
 type TodolistPropsType = {
   title: string
   tasks: Array<TaskType>
+  callBack: (id: number) => void
+  functionForButton: (value: FilterValuesType ) => void
 }
 
 export function Todolist(props: TodolistPropsType) {
@@ -20,15 +23,35 @@ export function Todolist(props: TodolistPropsType) {
         <button>+</button>
       </div>
       <ul>
-        <li><input type="checkbox" checked={props.tasks[0].isDone} /><span>{props.tasks[0].title}</span></li>
-        <li><input type="checkbox" checked={props.tasks[1].isDone} /><span>{props.tasks[1].title}</span></li>
-        <li><input type="checkbox" checked={props.tasks[2].isDone} /><span>{props.tasks[2].title}</span></li>
+        {props.tasks.map( i => <li key={i.id}>
+            <input type="checkbox" checked={i.isDone} />
+            <span>{i.title}</span>
+            <button onClick={ () => { props.callBack(i.id) } }>x</button>
+          </li>)}
       </ul>
       <div>
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+        <button onClick={ () => { props.functionForButton("all")}} >All</button>
+        <button onClick={ () => { props.functionForButton("active")}} >Active</button>
+        <button onClick={ () => { props.functionForButton("completed")}} >Completed</button>
       </div>
     </div>
   );
 }
+
+
+// Смысл useState и перерисовка согласно данным
+// 1. Добавляем onClick на кнопку
+// 2. Создаем функцию callBack в месте, где лежат данные, для onClick
+// 3. Передаем эту функцию ферез пропсы к месту, гд ележит кнопка. Не забываем передать ей данные
+// 4. Cоздаем useState для того, чтобы следить за нашими данными
+// 5. Ложим setTasks-функцию в нашу функцию для onClick, чтобы наши данные менялись
+// ТО ЕСТЬ: у нас есть функция useState, которая следит за данными в нашей искомой большой компоненте и передается в другую функцию в подкомпоненте, которая уже вызывается при определенном событии в JSX-разметке (эту функцию мы передаем туда с помощью пропсов). И все эти данные не забываем типизировать. Исходя из события, происходит изменение данных, что приводит к новой отрисовке JSX на странице. Так и меняются компоненты. Это как цикл, круговорот выполнения функций.
+
+
+// Меняем данные по нажатию на кнопку
+// 1. Сначала делаем данные в исходной большой компоненте
+// 2. Создаем useState привязанный к значениям наших кнопок
+// 3. Создаем filter на разные значения
+// 4. Передаем конечный массив данных в нашу JSX разметку через пропсы по обработчику событий
+// 5. Проверяем нашу отрисовку в зависимости от данных
+// Не забываем типизировать наши пропсы и данные, чтобы избежать ошибок
