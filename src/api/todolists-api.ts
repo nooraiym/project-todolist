724d517ea381d9cdb614d215f2157668c1b5be78
nooraiym <140681537+nooraiym@users.noreply.github.com>
2024-05-31 12:41:08 +0500

    fix: refactor requests to API

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -12,7 +12,7 @@ type FieldErrorType = {
     field: string
 }
 
-type ResponseType<D> = {
+type ResponseType<D = {}> = {
     resultCode: number
     messages: string[]
     fieldsErrors: FieldErrorType[]
@@ -20,43 +20,25 @@ type ResponseType<D> = {
 }
 
 
-
-const settings = {
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
         "API-KEY": "37c959d5-1aad-482b-a6de-5764ea80faf4"
-    }
-}
+    },
+})
 
 export const todolistsAPI = {
     getTodolists() {
-        const promise = axios.get<Array<TodolistAPIType>>(
-            'https://social-network.samuraijs.com/api/1.1/todo-lists',
-            settings
-        )
-        return promise
+        return instance.get<Array<TodolistAPIType>>('todo-lists')
     },
     createTodolist(title: string) {
-        const promise = axios.post<ResponseType<{ item: TodolistAPIType }>>(
-            'https://social-network.samuraijs.com/api/1.1/todo-lists',
-            { title }, 
-            settings
-        )
-        return promise
+        return instance.post<ResponseType<{item: TodolistAPIType}>>('todo-lists', {title})
     },
     deleteTodolist(todoID: string) {
-        const promise = axios.delete<ResponseType<{}>>(
-            `https://social-network.samuraijs.com/api/1.1/todo-lists/${todoID}`,
-            settings
-        )
-        return promise
+        return instance.delete<ResponseType>(`todo-lists/${todoID}`)
     },
     updateTodolists(todoID: string, title: string) {
-    const promise = axios.put<ResponseType<{}>>(
-            `https://social-network.samuraijs.com/api/1.1/todo-lists/${todoID}`,
-            { title },
-            settings
-        )
-    return promise
+        return instance.put<ResponseType>(`todo-lists/${todoID}`, {title})
     },
 }
